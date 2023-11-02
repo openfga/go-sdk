02addf98251d25bfc39b882fbe152fcbdd55d980
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2023-11-02 13:39:30 -0400

    release(v0.3.0-beta.1): wih initial support for conditions

--- configuration.go
+++ configuration.go
@@ -20,9 +20,9 @@ import (
 )
 
 const (
-	SdkVersion = "0.2.3"
+	SdkVersion = "0.3.0-beta.1"
 
-	defaultUserAgent = "openfga-sdk go/0.2.3"
+	defaultUserAgent = "openfga-sdk go/0.3.0-beta.1"
 )
 
 // RetryParams configures configuration for retry in case of HTTP too many request
