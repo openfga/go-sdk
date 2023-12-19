121eae90d97143d5003ff2847dd164acb8515aa7
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2023-12-19 14:37:14 -0500

    release(v0.3.1): support oauth2 client creds & fix oauth2 pkgs import path (#65)

--- configuration.go
+++ configuration.go
@@ -20,9 +20,9 @@ import (
 )
 
 const (
-	SdkVersion = "0.3.0"
+	SdkVersion = "0.3.1"
 
-	defaultUserAgent = "openfga-sdk go/0.3.0"
+	defaultUserAgent = "openfga-sdk go/0.3.1"
 )
 
 // RetryParams configures configuration for retry in case of HTTP too many request
