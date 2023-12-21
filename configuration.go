4285734d463d73a1b8caf1c8275fe1023d60f6c2
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2023-12-21 11:24:35 -0500

    release(v0.3.3): with WriteAuthorizationModel fix

--- configuration.go
+++ configuration.go
@@ -20,9 +20,9 @@ import (
 )
 
 const (
-	SdkVersion = "0.3.2"
+	SdkVersion = "0.3.3"
 
-	defaultUserAgent = "openfga-sdk go/0.3.2"
+	defaultUserAgent = "openfga-sdk go/0.3.3"
 )
 
 // RetryParams configures configuration for retry in case of HTTP too many request
