f9c1c963ebbbd6987f6c1c1c19357aeab4ed4e41
Ewan Harris <ewan.harris@okta.com>
2024-09-23 16:58:36 +0100

    chore: bump sdk version written in code (#125)

--- configuration.go
+++ configuration.go
@@ -20,9 +20,9 @@ import (
 )
 
 const (
-	SdkVersion = "0.6.0"
+	SdkVersion = "0.6.1"
 
-	defaultUserAgent = "openfga-sdk go/0.6.0"
+	defaultUserAgent = "openfga-sdk go/0.6.1"
 )
 
 // RetryParams configures configuration for retry in case of HTTP too many request
